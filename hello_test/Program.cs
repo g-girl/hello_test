acc2bf1df0d0e0f4a1837e0335a2420a43182413
Genesis Cruz <genesis.cruz@allata.com>
2018-09-25 18:16:58 -0600

    Random Number Generator Console app

--- hello_test/Program.cs
+++ hello_test/Program.cs
@@ -6,7 +6,42 @@ namespace hello_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            int random = rnd.Next(1, 10);
+            int userNumber;
+            Console.WriteLine("Input your chosen number");
+            var userInput = Console.ReadLine();
+            userNumber = int.Parse(userInput);
+            while (true) {
+                
+                
+                if (random == userNumber) {
+                    Console.WriteLine("Goodjob!");
+                    Console.ReadLine();
+                    break;
+
+                }
+                if (random < userNumber) {
+                    Console.WriteLine("Try a lower number!");
+                    Console.ReadLine();
+
+                }
+
+                if (random > userNumber) {
+                    Console.WriteLine("Try a higher number!");
+                    Console.ReadLine();
+
+                }
+
+
+                Console.ReadLine();
+            }
+            
+
+
+
+
+
         }
     }
 }
