f7510fd0ce76c7df89f561901b63a04beeca6367
Genesis Cruz <genesis.cruz@allata.com>
2018-09-30 19:55:17 -0600

    Rocked the random number generator

--- hello_test/Program.cs
+++ hello_test/Program.cs
@@ -1,47 +1,50 @@
 ﻿using System;
+using System.Linq;
 
 namespace hello_test
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Random rnd = new Random();
-            int random = rnd.Next(1, 10);
-            int userNumber;
-            Console.WriteLine("Input your chosen number");
-            var userInput = Console.ReadLine();
-            userNumber = int.Parse(userInput);
-            while (true) {
-                
-                
-                if (random == userNumber) {
-                    Console.WriteLine("Goodjob!");
+        private static void Main() {
+            Random random = new Random();
+            int computerNumber = random.Next(1, 10);
+
+            Console.WriteLine("Input a number: ");
+            
+            int count = 0;
+
+            bool playing = true;
+            while (playing) {
+                string userInput = Console.ReadLine();
+                count++;
+
+                int.TryParse(userInput, out int userNumber);
+
+                if (computerNumber == userNumber) {
+                    Console.WriteLine("You win! You are awesome, stay classy and in school");
+                    var tryPlurality = count == 1 ? "try" : "tries";
+                 
+                    Console.WriteLine($"It took you {count} {tryPlurality}!");
                     Console.ReadLine();
                     break;
-
                 }
-                if (random < userNumber) {
-                    Console.WriteLine("Try a lower number!");
-                    Console.ReadLine();
-
+                else if (computerNumber > userNumber) {
+                    Console.WriteLine("Higher");
+                    continue;
                 }
-
-                if (random > userNumber) {
-                    Console.WriteLine("Try a higher number!");
-                    Console.ReadLine();
-
+                else if (computerNumber < userNumber) {
+                    Console.WriteLine("Lower");
+                    continue;
                 }
 
-
-                Console.ReadLine();
             }
-            
-
-
-
-
 
         }
+        
+
+
+
+
+        
     }
 }
